19752a90c44c2975193521c646dda9eea5aaef94
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-01-15 19:08:36 +0700

    fix: remove not existed api

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from .client_app import client_bp
+# from api import api_bp
 
 app = Flask(__name__, static_folder="../dist/")
-app.register_blueprint(client_bp)
+# app.register_blueprint(api_bp)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
