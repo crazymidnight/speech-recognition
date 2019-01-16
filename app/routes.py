3e8fe2936bd04943526eb2c752b176ad40309cf5
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-01-16 15:53:27 +0700

    feat: add file upload route

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import current_app, send_file, render_template
+from flask import current_app, send_file, request, abort, jsonify
 import os
 from app import app
 
@@ -9,3 +9,17 @@ def index():
     dist_dir = current_app.config["DIST_DIR"]
     entry = os.path.join(dist_dir, "index.html")
     return send_file(entry)
+
+
+@app.route("/api/recognize/", methods=["POST", "GET"])
+def upload():
+    if request.method == "POST":
+        f = request.files["file"]
+        print(f.name)
+
+        file_path = os.path.join("data", f.name)
+        f.save(f"{file_path}.wav")
+
+        return jsonify({"msg": "hello"}), 201
+
+    abort(405)
